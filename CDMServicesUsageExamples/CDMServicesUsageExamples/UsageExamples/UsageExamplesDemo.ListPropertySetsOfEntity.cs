45934a037aa0a7a188e29f42a427676ea58f9f76
aswinraghavendraa_rv <aswinraghavendraa_rv@trimble.com>
2025-07-08 13:43:33 +0530

    Changes in CDM Services Usage Sample
    Updated CDM Services Usage Sample

---
+++ CDMServicesUsageExamples/CDMServicesUsageExamples/UsageExamples/UsageExamplesDemo.ListPropertySetsOfEntity.cs
@@ -0,0 +1,89 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UsageExamplesDemo.ListPRopertySetsOfEntity.cs" company="Trimble Inc.">
+//     Copyright (c) Trimble Inc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CDMServicesUsageExamples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Trimble.Connect.PSet.Client;
+
+    /// <summary>
+    /// The usage examples demo.
+    /// This part of the class demonstrates listing all the property sets of an entity.
+    /// </summary>
+    public partial class UsageExamplesDemo
+    {
+        /// <summary>
+        /// The format of the entity FRN links.
+        /// </summary>
+        private const string EntityLinkPattern = "frn:entity:{0}"; // Where {0} is the ID of the entity
+
+        /// <summary>
+        /// List all the property sets associated with a given entity within a given project.
+        /// This approach requests all the property sets from the service which are associated with a link
+        /// that is formed using the specified entity's ID and then filters the results by the library IDs
+        /// which are associated with the specified project.
+        /// An alternative approach would be to first request all the PSet definitions from all the project libraries
+        /// and then iterate over the definitions and for each of them request all the PSets (using library ID, definition ID and link).
+        /// Which approach is more optimal depends on the data which exists in the service.
+        /// In the case when the specified entity exists in multiple projects and has many PSets associated with it,
+        /// but in the specified project there are not many PSet definitions, the alternative approach might be more optimal.
+        /// Otherwise, if there are many PSet definitions in the specified project overall
+        /// (not just for the PSets associated with the specified entity), but the specified entity only exists in a few projects,
+        /// the approach demonstrated here is more optimal.
+        /// </summary>
+        /// <param name="projectId">The ID of the project.</param>
+        /// <param name="entityId">The ID of the entity.</param>
+        private async Task ListPropertySetsOfEntityInProject(string projectId, string entityId)
+        {
+            // Get the list of the IDs of all the libraries which belong to the specified project.
+            // We will use this list of library IDs to filter the property sets
+            // because we only need the property sets which belong to the specified project (to libraries of the specified project).
+            IList<string> projectLibraryIDs = await GetAllProjectLibraries(projectId).ConfigureAwait(false);
+
+            // Get all the psets which are associated with a link that contains the specified entity's ID.
+            var listPSetsOfLinkRequest = new ListPSetsOfLinkRequest
+            {
+                // FRN links may not contain special characters. See: https://drive.google.com/file/d/1gR1PEhrR58WGlrZt8PPWqTPbjvbEwXVK/view
+                Link = string.Format(EntityLinkPattern, Uri.EscapeDataString(entityId)),
+            };
+
+            Console.WriteLine($"Listing all PSets associated with the link {listPSetsOfLinkRequest.Link}...");
+
+            try
+            {
+            await this.psetClient.ListAllPSetsOfLinkAsync(listPSetsOfLinkRequest,
+                (PSetsPage psetsPage) =>
+                {
+                    if (psetsPage != null && psetsPage.Items != null)
+                    {
+                        foreach (var pset in psetsPage.Items)
+                        {
+                            // Filter the returned PSets to keep only the ones which are in the libraries of the specified project.
+                            // This is needed because the ListAllPSetsOfLinkAsync() call returns all the psets associated with the specified entity link,
+                            // from all libraries, even if those libraries don't belong to the specified project.
+                            if (projectLibraryIDs.Contains(pset.LibraryId))
+                            {
+                                PrintPSet(pset);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Filtering out PSet with LibraryId={pset.LibraryId}, DefinitionID={pset.DefinitionId}, Link={pset.Link} because it is not in the project {projectId}.");
+                            }
+                        }
+                    }
+                }).ConfigureAwait(false);
+             }
+            catch(Exception ex)
+            {
+                // Handle the exception as required.
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ CDMServicesUsageExamples/CDMServicesUsageExamples/UsageExamples/UsageExamplesDemo.cs
@@ -0,0 +1,159 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UsageExamplesDemo.cs" company="Trimble Inc.">
+//     Copyright (c) Trimble Inc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CDMServicesUsageExamples
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Trimble.Connect.Client;
+    using Trimble.Connect.Org.Client;
+    using Trimble.Connect.PSet.Client;
+
+    /// <summary>
+    /// Usage examples demonstration.
+    /// This part of the class contains basic functionality.
+    /// </summary>
+    public partial class UsageExamplesDemo
+    {
+        /// <summary>
+        /// The Organizer service client.
+        /// </summary>
+        private IOrgClient orgClient;
+
+        /// <summary>
+        /// The Property Set service client.
+        /// </summary>
+        private IPSetClient psetClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsageExamplesDemo"/> class.
+        /// </summary>
+        /// <param name="credentialsProvider">The credentials provider.</param>
+        public UsageExamplesDemo(ICredentialsProvider credentialsProvider)
+        {
+            if (credentialsProvider is null)
+            {
+                throw new ArgumentNullException(nameof(credentialsProvider));
+            }
+
+            // Create the Organizer service client based on the Organizer service URL specified in the Config
+            // and based on the credentials provider that was previously created.
+            this.orgClient = new OrgClient(
+                new OrgClientConfig { ServiceURI = new Uri(Config.OrgServiceUrl) },
+                credentialsProvider);
+
+            // Create the Property Set service client based on the Property Set service URL specified in the Config
+            // and based on the credentials provider that was previously created.
+            this.psetClient = new PSetClient(
+                new PSetClientConfig { ServiceURI = new Uri(Config.PSetServiceUrl) },
+                credentialsProvider);
+        }
+
+        /// <summary>
+        /// The main method demonstrating the usage examples.
+        /// </summary>
+        /// <returns>Does not return anything.</returns>
+        public async Task Run()
+        {
+            Console.WriteLine("----- Starting the usage examples demo... -----");
+            Console.WriteLine("Disclaimer: some of the demos contain additional information on conventions used for early adopters, information is bound to change.");
+            Console.WriteLine();
+
+            Console.WriteLine("Please choose the functionality to demonstrate:");
+            Console.WriteLine("1. Property set library discovery.");
+            Console.WriteLine("2. Creating a property set library and linking it into the project discovery tree.");
+            Console.WriteLine("3. Listing of the property sets of an entity.");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine();
+
+            switch (keyInfo.KeyChar)
+            {
+                case '1':
+            Console.WriteLine("Demonstrating how to discover a PSet library based on a project ID and a library name...");
+            await LibraryDiscoveryDemo().ConfigureAwait(false);
+                    break;
+                case '2':
+                    Console.WriteLine("Demonstrating creating a PSet library and linking it into the project discovery tree...");
+                    await LibraryCreationAndLinkingIntoProjectDiscoveryTreeDemo().ConfigureAwait(false);
+                    break;
+                case '3':
+            Console.WriteLine("Demonstrating how to list all the property sets which belong to a specified entity in a specified project...");
+            await ListPropertySetsOfEntityDemo().ConfigureAwait(false);
+                    break;
+                default:
+                    Console.WriteLine("Unknown option picked: " + keyInfo.KeyChar + ".");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("----- Finished the usage examples .NET SDK demo. -----");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Demonstrate how to discover a PSet library based on a project ID and a library name.
+        /// </summary>
+        /// <returns>Does not return anything.</returns>
+        private async Task LibraryDiscoveryDemo()
+        {
+            Console.Write("Please enter the project ID which contains the PSet library to discover: ");
+            string projectId = Console.ReadLine();
+            Console.Write("Please enter the name of the PSet library to discover: ");
+            string libraryName = Console.ReadLine();
+
+            string libraryId = await this.DiscoverProjectLibrary(projectId, libraryName).ConfigureAwait(false);
+            if (string.IsNullOrEmpty(libraryId))
+            {
+                Console.WriteLine($"No library named {libraryName} exists in the project {projectId}.");
+            }
+            else
+            {
+                Console.WriteLine($"The ID of the library named {libraryName} in the project {projectId} is: {libraryId}.");
+            }
+        }
+
+        /// <summary>
+        /// Demonstrate how to create a PSet library and link it into the project discovery tree.
+        /// </summary>
+        /// <returns>Does not return anything.</returns>
+        private async Task LibraryCreationAndLinkingIntoProjectDiscoveryTreeDemo()
+        {
+            Console.Write("Please enter the project ID to which the new library should be linked: ");
+            string projectId = Console.ReadLine();
+            Console.Write("Please enter the name of the PSet library to create: ");
+            string libraryName = Console.ReadLine();
+
+            await this.CreateLibraryAndLinkIntoProjectDiscoveryTree(projectId, libraryName).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Demonstrate how to list all the property sets of an entity in a project.
+        /// </summary>
+        /// <returns>Does not return anything.</returns>
+        private async Task ListPropertySetsOfEntityDemo()
+        {
+            Console.Write("Please enter the ID of the project in which the property sets exist: ");
+            string projectId = Console.ReadLine();
+            Console.Write("Please enter the ID of the entity for which to list the property sets: ");
+            string entityID = Console.ReadLine();
+
+            await ListPropertySetsOfEntityInProject(projectId, entityID).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Obtains the ID of the user who is running the demo.
+        /// </summary>
+        /// <returns>The user's ID.</returns>
+        private async Task<string> GetUserID()
+        {
+            var userClaims = await this.psetClient.GetMeAsync().ConfigureAwait(false);
+
+            return userClaims.Sub;
+        }
+    }
+}
